2ff011925aa4432c21383cc12237930c8f7500b2
EC2 Default User <ec2-user@ip-172-31-87-77.ec2.internal>
2023-03-12 03:50:16 +0000

    make_edit_user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,5 +5,22 @@ class UsersController < ApplicationController
   end
 
   def edit
+    @user = User.find(params[:id])
+    unless @user.id == current_user.id
+      redirect_to user_path(@user.id)
+    end
   end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(@user.id)
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :profile_image)
+  end
+
 end
